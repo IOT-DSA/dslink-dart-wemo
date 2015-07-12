62d76e554238ef2a754e07d3e9c544f41c681b44
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-12 01:02:19 -0400

    Add Add Device Action

--- bin/run.dart
+++ bin/run.dart
@@ -1,6 +1,7 @@
 import "dart:async";
 
 import "package:dslink/dslink.dart";
+import "package:dslink/nodes.dart";
 
 import "package:upnp/upnp.dart";
 
@@ -9,7 +10,63 @@ LinkProvider link;
 SimpleNode devicesNode;
 
 main(List<String> args) async {
-  link = new LinkProvider(args, "WeMo-", isResponder: true, command: "run", defaultNodes: {
+  link = new LinkProvider(args, "WeMo-", isResponder: true, command: "run", profiles: {
+    "getBinaryState": (String path) => new GetBinaryStateNode(path),
+    "setBinaryState": (String path) => new SetBinaryStateNode(path),
+    "toggleBinaryState": (String path) => new ToggleBinaryStateNode(path),
+    "brewCoffee": (String path) => new BrewCoffeeNode(path),
+    "addDevice": (String path) => new SimpleActionNode(path, (Map<String, dynamic> params) async {
+      var ip = params["ip"];
+
+      if (ip == null || ip is! String) {
+        return {
+          "success": false,
+          "error": "Bad IP"
+        };
+      }
+
+      var port = params["port"];
+
+      try {
+        if (port is String) {
+          port = int.parse(port);
+        }
+
+        var dm = new DiscoveredClient();
+        dm.location = "http://${ip}:${port}/setup.xml";
+
+        var device = await dm.getDevice();
+
+        await addDevice(device, true);
+        await link.saveAsync();
+      } catch (e) {
+        return {
+          "success": false,
+          "error": e.toString()
+        };
+      }
+
+      return {
+        "success": true,
+        "error": ""
+      };
+    }),
+    "remove": (String path) => new DeleteActionNode.forParent(path, link.provider)
+  }, autoInitialize: false);
+
+  link.init();
+
+  link.onValueChange("/Auto_Discovery").listen((_) async {
+    await link.saveAsync();
+  });
+
+  var m = {
+    "Auto_Discovery": {
+      r"$name": "Auto Discovery",
+      r"$type": "bool[disabled,enabled]",
+      "?value": true,
+      r"$writable": "write"
+    },
     "Value_Tick_Rate": {
       r"$name": "Value Tick Rate",
       r"$type": "number",
@@ -22,16 +79,42 @@ main(List<String> args) async {
       r"$type": "number",
       r"$unit": "seconds",
       r"$writable": "write",
-      "?value": 20
+      "?value": 30
+    },
+    "Add_Device": {
+      r"$name": "Add Device",
+      r"$is": "addDevice",
+      r"$invokable": "write",
+      r"$result": "values",
+      r"$params": [
+        {
+          "name": "ip",
+          "type": "string"
+        },
+        {
+          "name": "port",
+          "type": "int",
+          "default": 49154
+        }
+      ],
+      r"$columns": [
+        {
+          "name": "success",
+          "type": "bool"
+        },
+        {
+          "name": "error",
+          "type": "string"
+        }
+      ]
     }
-  }, profiles: {
-    "getBinaryState": (String path) => new GetBinaryStateNode(path),
-    "setBinaryState": (String path) => new SetBinaryStateNode(path),
-    "toggleBinaryState": (String path) => new ToggleBinaryStateNode(path),
-    "brewCoffee": (String path) => new BrewCoffeeNode(path)
-  }, autoInitialize: false);
+  };
 
-  link.init();
+  for (var n in m.keys) {
+    if (!link.provider.nodes.containsKey("/${n}")) {
+      link.addNode("/${n}", m[n]);
+    }
+  }
 
   devicesNode = link["/"];
 
@@ -88,10 +171,14 @@ Timer valueUpdateTimer;
 Timer discoveryTimer;
 
 updateDevices() async {
+  if (link.val("/Auto_Discovery_Enabled") != true) {
+    return;
+  }
+
   List<Device> devices;
 
   try {
-    devices = await discoverer.getDevices(type: CommonDevices.WEMO).timeout(new Duration(seconds: 10), onTimeout: () {
+    devices = await discoverer.getDevices(type: CommonDevices.WEMO).timeout(new Duration(seconds: 15), onTimeout: () {
       return [];
     });
   } catch (e) {
@@ -108,123 +195,143 @@ updateDevices() async {
   }
 
   for (Device device in devices) {
-    if ((link.provider as NodeProviderImpl).nodes.containsKey("/${device.uuid}")) {
+    if (
+      (link.provider as NodeProviderImpl).nodes.containsKey("/${device.uuid}") &&
+      link["/${device.uuid}"].children.containsKey("BinaryState")) {
       continue;
     }
 
     print("Discovered Device: ${device.friendlyName}");
 
-    var m = {
-      r"$name": device.friendlyName,
-      r"$uuid": device.uuid
-    };
-
-    var basicEventService = await device.getService("urn:Belkin:service:basicevent:1");
-
-    try {
-      var deviceEventService = await device.getService("urn:Belkin:service:deviceevent:1");
-      deviceEventServices["/${device.uuid}"] = deviceEventService;
-    } catch (e) {
-    }
-
-    basicEventServices["/${device.uuid}"] = basicEventService;
-
-    m["BinaryState"] = {
-      r"$type": "int",
-      r"$name": "Binary State"
-    };
-
-    m["Model_Name"] = {
-      r"$type": "string",
-      r"$name": "Model Name",
-      r"?value": device.modelName
-    };
-
-    m["Manufacturer"] = {
-      r"$type": "string",
-      r"?value": device.manufacturer,
-    };
-
-    m["BinaryState"]["Get"] = {
-      r"$is": "getBinaryState",
-      r"$invokable": "read",
-      r"$result": "values",
-      r"$columns": [
-        {
-          "name": "state",
-          "type": "int"
-        }
-      ]
-    };
-
-    m["BinaryState"]["Set"] = {
-      r"$is": "setBinaryState",
-      r"$invokable": "write",
-      r"$result": "values",
-      r"$params": [
-        {
-          "name": "state",
-          "type": "int"
-        }
-      ],
-      r"$columns": {}
-    };
-
-    m["BinaryState"]["Toggle"] = {
-      r"$is": "toggleBinaryState",
-      r"$invokable": "write",
-      r"$result": "values",
-      r"$params": {},
-      r"$columns": {}
-    };
-
-    if (device.modelName == "CoffeeMaker") {
-      m[r"$isCoffeeMaker"] = true;
-
-      m["Mode"] = {
-        r"$type": "string",
-        "?value": "Unknown"
-      };
-
-      m["Brew_Coffee"] = {
-        r"$name": "Brew Coffee",
-        r"$is": "brewCoffee",
-        r"$invokable": "write",
-        r"$columns": {},
-        r"$params": {},
-        r"result": "values"
-      };
-
-      m["Brew_Started"] = {
-        r"$name": "Brew Started",
-        r"$type": "string",
-        "?value": "Unknown"
-      };
-
-      m["Brew_Completed"] = {
-        r"$name": "Brew Completed",
-        r"$type": "string",
-        "?value": "Unknown"
-      };
-
-      m["Brew_Duration"] = {
-        r"$name": "Brew Duration",
-        r"$type": "int",
-        "?value": -1
-      };
-
-      m["Brew_Age"] = {
-        r"$name": "Brew Age",
-        r"$type": "int",
-        "?value": -1
-      };
-    } else {
-      m[r"$isCoffeeMaker"] = false;
-    }
-    link.addNode("/${device.uuid}", m);
-    SimpleNode n = link["/${device.uuid}"];
-    n.serializable = false;
+    await addDevice(device);
   }
+}
+
+addDevice(Device device, [bool manual = false]) async {
+  if (
+    (link.provider as NodeProviderImpl).nodes.containsKey("/${device.uuid}") &&
+    link["/${device.uuid}"].children.containsKey("BinaryState")) {
+    throw "Device already added.";
+  }
+
+  var m = {
+    r"$name": device.friendlyName,
+    r"$uuid": device.uuid
+  };
+
+  var basicEventService = await device.getService("urn:Belkin:service:basicevent:1");
+
+  try {
+    var deviceEventService = await device.getService("urn:Belkin:service:deviceevent:1");
+    deviceEventServices["/${device.uuid}"] = deviceEventService;
+  } catch (e) {
+  }
+
+  basicEventServices["/${device.uuid}"] = basicEventService;
+
+  if (manual) {
+    m["Remove"] = {
+      r"$invokable": "write",
+      r"$is": "remove",
+      r"$result": "values"
+    };
+  }
+
+  m["BinaryState"] = {
+    r"$type": "int",
+    r"$name": "Binary State"
+  };
+
+  m["Model_Name"] = {
+    r"$type": "string",
+    r"$name": "Model Name",
+    r"?value": device.modelName
+  };
+
+  m["Manufacturer"] = {
+    r"$type": "string",
+    r"?value": device.manufacturer,
+  };
+
+  m["BinaryState"]["Get"] = {
+    r"$is": "getBinaryState",
+    r"$invokable": "read",
+    r"$result": "values",
+    r"$columns": [
+      {
+        "name": "state",
+        "type": "int"
+      }
+    ]
+  };
+
+  m["BinaryState"]["Set"] = {
+    r"$is": "setBinaryState",
+    r"$invokable": "write",
+    r"$result": "values",
+    r"$params": [
+      {
+        "name": "state",
+        "type": "int"
+      }
+    ],
+    r"$columns": {}
+  };
+
+  m["BinaryState"]["Toggle"] = {
+    r"$is": "toggleBinaryState",
+    r"$invokable": "write",
+    r"$result": "values",
+    r"$params": {},
+    r"$columns": {}
+  };
+
+  if (device.modelName == "CoffeeMaker") {
+    m[r"$isCoffeeMaker"] = true;
+
+    m["Mode"] = {
+      r"$type": "string",
+      "?value": "Unknown"
+    };
+
+    m["Brew_Coffee"] = {
+      r"$name": "Brew Coffee",
+      r"$is": "brewCoffee",
+      r"$invokable": "write",
+      r"$columns": {},
+      r"$params": {},
+      r"result": "values"
+    };
+
+    m["Brew_Started"] = {
+      r"$name": "Brew Started",
+      r"$type": "string",
+      "?value": "Unknown"
+    };
+
+    m["Brew_Completed"] = {
+      r"$name": "Brew Completed",
+      r"$type": "string",
+      "?value": "Unknown"
+    };
+
+    m["Brew_Duration"] = {
+      r"$name": "Brew Duration",
+      r"$type": "int",
+      "?value": -1
+    };
+
+    m["Brew_Age"] = {
+      r"$name": "Brew Age",
+      r"$type": "int",
+      "?value": -1
+    };
+  } else {
+    m[r"$isCoffeeMaker"] = false;
+  }
+  link.addNode("/${device.uuid}", m);
+  SimpleNode n = link["/${device.uuid}"];
+  n.serializable = manual;
 }
 
 Duration deviceDiscoveryTickRate;
